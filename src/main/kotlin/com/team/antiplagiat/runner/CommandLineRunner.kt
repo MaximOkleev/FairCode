a34e99e05f4801ccdd52e3902578d8b4070fef19
Ilya-Lavr <mailtoakkfogame@gmail.com>
2026-02-02 23:02:07 +0300

    Logger in interface and my CRUD

---
+++ src/main/kotlin/com/team/antiplagiat/runner/CommandLineRunner.kt
@@ -0,0 +1,5 @@
+package com.team.antiplagiat.runner
+
+class CommandLineRunner {
+
+}
--- src/main/kotlin/com/team/antiplagiat/service/UserCRUD.kt
+++ src/main/kotlin/com/team/antiplagiat/service/UserCRUD.kt
@@ -1,12 +1,14 @@
 package com.team.antiplagiat.service
 
 import com.team.antiplagiat.models.user.User
+import org.springframework.stereotype.Service
 
+@Service
 class UserCRUD : ServiceCRUD<User> {
 
     override val entities: MutableMap<Long, User> = mutableMapOf()
 
-    fun update(id : Long, login : String?, email: String?): Boolean {
+    fun update(id: Long, login: String?, email: String?): Boolean {
         val user = entities[id] ?: return false
         if (login != null) user.login = login
         if (email != null) user.email = email
