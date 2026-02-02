953f5d66f03a53134c72a24644d89b7a2b89a7b9
Maksim <mak.okleev@gmail.com>
2026-02-02 23:27:11 +0300

    Максим дз на 02.02

--- src/main/kotlin/com/team/antiplagiat/runner/CommandLineRunner.kt
+++ src/main/kotlin/com/team/antiplagiat/runner/CommandLineRunner.kt
@@ -3,54 +3,84 @@ package com.team.antiplagiat.runner
 import com.team.antiplagiat.models.user.User
 import com.team.antiplagiat.models.user.service.UserCRUD
 import com.team.antiplagiat.service.ProblemCrudService
-import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 
 @Component
-class CommandLineRunnner(
+class CommandLineRunner(
     private val problemService: ProblemCrudService,
     private val userService: UserCRUD
 ) : CommandLineRunner {
 
-    private val logger = LoggerFactory.getLogger(CommandLineRunnner::class.java)
-
     override fun run(vararg args: String?) {
-        logger.info("\n" + "=".repeat(70))
-        logger.info("ДЕМОНСТРАЦИЯ РАБОТЫ CRUD СЕРВИСОВ")
-        logger.info("=".repeat(70))
+        println("=".repeat(70))
+        println("ДЕМОНСТРАЦИЯ РАБОТЫ CRUD СЕРВИСОВ")
+        println("=".repeat(70))
 
-        // Часть 1: Демонстрация работы с UserCRUD
-        logger.info("\n" + "-".repeat(30))
-        logger.info("ЧАСТЬ 1: РАБОТА С UserCRUD")
-        logger.info("-".repeat(30))
+        println("\nЧАСТЬ 1: ДЕМОНСТРАЦИЯ UserCRUD")
+        println("-".repeat(30))
 
-        // Создаем пользователей
-        logger.info("\n1. СОЗДАНИЕ ПОЛЬЗОВАТЕЛЕЙ:")
+        // CREATE
+        println("\n1. СОЗДАНИЕ ПОЛЬЗОВАТЕЛЕЙ:")
         val user1 = User(1, "user1", "user1@example.com")
         val user2 = User(2, "user2", "user2@example.com")
 
-        val user1Created = userService.create(user1)
-        val user2Created = userService.create(user2)
+        userService.create(user1)
+        userService.create(user2)
 
-        logger.info("Пользователь 1 создан: $user1Created")
-        logger.info("Пользователь 2 создан: $user2Created")
+        // READ
+        println("\n2. ЧТЕНИЕ ПОЛЬЗОВАТЕЛЯ:")
+        val foundUser = userService.read(1)
+        println("Найден пользователь: ${foundUser?.login} (email: ${foundUser?.email})")
 
-        // Часть 2: Демонстрация работы с ProblemCrudService
-        logger.info("\n\n" + "-".repeat(30))
-        logger.info("ЧАСТЬ 2: РАБОТА С ProblemCrudService")
-        logger.info("-".repeat(30))
+        // UPDATE
+        println("\n3. ОБНОВЛЕНИЕ ПОЛЬЗОВАТЕЛЯ:")
+        userService.update(1, "updated_user", "new_email@example.com")
 
-        // Создание задач
-        logger.info("\n1. СОЗДАНИЕ ЗАДАЧ:")
-        val task1Created = problemService.createProblem("Сумма чисел", "Найти сумму двух чисел")
-        logger.info("Задача 'Сумма чисел' создана: $task1Created")
+        // Проверка обновления
+        val updatedUser = userService.read(1)
+        println("Обновленный email: ${updatedUser?.email}")
 
-        val task2Created = problemService.createProblem("Факториал", "Вычислить факториал числа")
-        logger.info("Задача 'Факториал' создана: $task2Created")
+        // DELETE
+        println("\n4. УДАЛЕНИЕ ПОЛЬЗОВАТЕЛЯ:")
+        userService.delete(2)
 
-        logger.info("\n" + "=".repeat(70))
-        logger.info("ДЕМОНСТРАЦИЯ ЗАВЕРШЕНА")
-        logger.info("=".repeat(70))
+        println("\nЧАСТЬ 2: ДЕМОНСТРАЦИЯ ProblemCrudService")
+        println("-".repeat(30))
+
+        // CREATE
+        println("\n1. СОЗДАНИЕ ЗАДАЧ:")
+        problemService.createProblem("Сумма чисел", "Найти сумму двух чисел")
+        problemService.createProblem("Факториал", "Вычислить факториал числа")
+        problemService.createProblem("Палиндром", "Проверить строку на палиндром")
+
+        // READ
+        println("\n2. ЧТЕНИЕ ВСЕХ ЗАДАЧ:")
+        val allProblems = problemService.getAllProblems()
+        println("Всего задач: ${allProblems.size}")
+        allProblems.forEach { problem ->
+            println("  ID: ${problem.id}, Название: '${problem.name}'")
+        }
+
+        // READ BY ID
+        println("\n3. ЧТЕНИЕ ЗАДАЧИ ПО ID:")
+        val problem = problemService.read(1)
+        println("Задача с ID=1: ${problem?.name} - ${problem?.description}")
+
+        // UPDATE
+        println("\n4. ОБНОВЛЕНИЕ ЗАДАЧИ:")
+        problemService.updateProblem(1, "Сумма двух чисел", "Сложить 2 числа")
+
+        // DELETE
+        println("\n5. УДАЛЕНИЕ ЗАДАЧИ:")
+        problemService.delete(2)
+
+        println("\n6. ФИНАЛЬНОЕ СОСТОЯНИЕ:")
+        val finalProblems = problemService.getAllProblems()
+        println("Осталось задач: ${finalProblems.size}")
+
+        println("\n" + "=".repeat(70))
+        println("ДЕМОНСТРАЦИЯ ЗАВЕРШЕНА")
+        println("=".repeat(70))
     }
 }
