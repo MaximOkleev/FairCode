fd1a1d6f3bfb162a6188b64b30428f3804c00674
Amira <akt383691@gmail.com>
2026-02-02 22:16:17 +0300

    solution crud1

--- src/main/kotlin/com/team/antiplagiat/service/AntiPlagiatProperties.kt
+++ src/main/kotlin/com/team/antiplagiat/service/AntiPlagiatProperties.kt
@@ -1,9 +1,8 @@
 package com.team.antiplagiat.service
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
 
 @ConfigurationProperties(prefix = "antiplagiat")
 class AntiPlagiatProperties {
-    val maxAttempts: Int = 0
+    var maxAttempts: Int = 50
 }
