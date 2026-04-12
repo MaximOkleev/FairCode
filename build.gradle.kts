6e65fd02e341113b88bc625a983f2dedca55908b
Maksim <mak.okleev@gmail.com>
2026-04-12 16:52:14 +0300

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
 	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -11,7 +11,7 @@ description = "Antiplagiat Project"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(25)
 	}
 }
 
--- src/main/kotlin/com/team/antiplagiat/controller/dto/ContestDto.kt
+++ src/main/kotlin/com/team/antiplagiat/controller/dto/ContestDto.kt
@@ -1,48 +1,37 @@
 package com.team.antiplagiat.controller.dto
 
-import com.team.antiplagiat.models.Solution
+import com.team.antiplagiat.models.Contest
+import com.team.antiplagiat.models.User
 import java.time.LocalDateTime
 
-data class NewSolutionResponse(
-    val solutionId: Long
+data class ContestRequest(
+    val name: String,
+    val adminId: Long,
+    val startedAt: LocalDateTime,
+    val duration: Int
 )
 
-data class NewSolutionRequest(
-    val userId: Long,
-    val taskId: Long,
-    val language: String,
-    val filePath: String
-)
-
-fun NewSolutionRequest.toEntity(): Solution {
-    return Solution(
-        id = 0,
-        userId = this.userId,
-        taskId = this.taskId,
-        language = this.language,
-        filePath = this.filePath,
-        date = LocalDateTime.now()
-    )
+data class ContestResponse(
+    val id: Long,
+    val name: String,
+    val adminId: Long,
+    val startedAt: LocalDateTime,
+    val duration: Int
+) {
+    companion object {
+        fun fromEntity(contest: Contest): ContestResponse = ContestResponse(
+            id = contest.id,
+            name = contest.name,
+            adminId = contest.admin.id,
+            startedAt = contest.startedAt,
+            duration = contest.duration
+        )
+    }
 }
 
-data class GetSolutionResponse(
-    val id: Long,
-    val userId: Long,
-    val taskId: Long,
-    val language: String,
-    val filePath: String,
-    val createdAt: LocalDateTime
-)
-
-fun Solution.toResponse() = GetSolutionResponse(
-    id = this.id,
-    userId = this.userId,
-    taskId = this.taskId,
-    language = this.language,
-    filePath = this.filePath,
-    createdAt = this.date
-)
-
-data class GetCountResponse(
-    val count: Int
+fun ContestRequest.toEntity(admin: User): Contest = Contest(
+    name = this.name,
+    admin = admin,
+    startedAt = this.startedAt,
+    duration = this.duration
 )
